4211b354c181cab034c58c296e260b0370152e28
Paulin Violette <violette.paulin@gmail.com>
2018-03-15 15:58:15 +0100

    lobbyScreen template

---
+++ core/src/fr/univangers/vajin/screens/LobbyScreen.java
@@ -0,0 +1,52 @@
+package fr.univangers.vajin.screens;
+
+import com.badlogic.gdx.Screen;
+import fr.vajin.snakerpg.gameroom.jsondatabeans.LobbyBean;
+
+public class LobbyScreen implements Screen {
+
+    LobbyBean lobbyBean;
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    public LobbyBean getLobbyBean() {
+        return lobbyBean;
+    }
+
+    public void setLobbyBean(LobbyBean lobbyBean) {
+        this.lobbyBean = lobbyBean;
+    }
+}
