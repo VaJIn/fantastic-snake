997d8e54090fa9810752d23e0dbc14bea4744b2b
valentine <valentine.rahier@etud.univ-angers.fr>
2018-03-15 15:18:45 +0100

    lobbypackethandler

--- core/src/fr/univangers/vajin/network/PacketHandler.java
+++ core/src/fr/univangers/vajin/network/PacketHandler.java
@@ -3,6 +3,8 @@ package fr.univangers.vajin.network;
 import java.net.DatagramPacket;
 
 public interface PacketHandler {
+    
+    
 
     void handlePacket(DatagramPacket packet);
 
---
+++ core/src/fr/univangers/vajin/network/impl/LobbyPacketHandler.java
@@ -0,0 +1,40 @@
+package fr.univangers.vajin.network.impl;
+
+import com.google.gson.Gson;
+import fr.univangers.vajin.network.PacketCreator;
+import fr.univangers.vajin.network.PacketHandler;
+import fr.univangers.vajin.network.jsondatabeans.LobbyBean;
+
+import java.net.DatagramPacket;
+import java.nio.ByteBuffer;
+
+public class LobbyPacketHandler implements PacketHandler {
+
+    private static int BUFFER_START_POS = 16;
+
+
+    @Override
+    public void handlePacket(DatagramPacket packet) {
+
+        ByteBuffer buffer = ByteBuffer.wrap(packet.getData());
+
+        buffer.position(BUFFER_START_POS);
+
+        int type = buffer.getInt();
+
+        if(type!= PacketCreator.GAMEROOM_DESC){
+            throw new IllegalArgumentException("Error packet type");
+        }
+
+        int jsonSize = buffer.getInt();
+
+        byte [] jsonData = new byte[jsonSize];
+
+        buffer.get(jsonData);
+
+        Gson gson = new Gson();
+
+        LobbyBean lobbyBean = gson.fromJson(String.valueOf(jsonData),LobbyBean.class);
+
+    }
+}
